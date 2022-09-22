dfebfa4278509ff5a923b312685ec2cee0651431
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-09-22 08:48:29 +0300

    test passing

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About ({
+    image="https://via.placeholder.com/215",alt,about}){
+    return(
+        <aside>
+            <img src={image} alt="blog logo"/>
+            <>
+            <p>{about}</p>
+            {/* <p>{about}</p> */}
+            </>
+        </aside>
+    )
+}
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header"
+import About from "./About"
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
 function App() {
+  // const blogName = "Overreacted"
+  // const alt = "blog logo"
+  // const aboutText ="Persomal blog by DanAbramov. I explain with words and code."
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header blogName={blogData.blogName}/>
+      <About image={blogData.image} alt={alt} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
+      {/* You're on your own from here! Follow the deliverables; test things out in
+      the browser as you write your code; and good luck! */}
     </div>
   );
 }
